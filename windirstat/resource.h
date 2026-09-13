3e20a2b5bbea773d3f21d8cb69a8e488ccb60b7f
marcmy <21000174+marcmy@users.noreply.github.com>
2026-09-13 14:05:44 -0400

    Resolve sync resource ID conflict

--- windirstat/resource.h
+++ windirstat/resource.h
@@ -1,4 +1,4 @@
-﻿//{{NO_DEPENDENCIES}}
+//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by windirstat.rc
 //
@@ -168,6 +168,7 @@
 #define IDC_REMOVE_CLEANUP              1149
 #define IDC_ADD_FOLDER                  1152
 #define IDC_TREEMAPPRESET                1153
+#define IDC_SAMPLE_LARGE_FILES           1154
 #define ID_WDS_CONTROL                  4711
 #define ID_TREEMAP_ZOOMRESET            32767
 #define ID_CLEANUP_EXPLORER_SELECT      32768
@@ -282,7 +283,8 @@
 #define ID_VIEW_TOOLBAR_SIZE_175        32973
 #define ID_VIEW_TOOLBAR_SIZE_200        32974
 #define ID_VIEW_TOOLBAR_SIZE_USE_WINDOWS 32975
-#define ID_TREEMAP_SHOW_FOLDER_SIZES    32976
+#define ID_WATCHER_FILTER               32976
+#define ID_TREEMAP_SHOW_FOLDER_SIZES    32977
 #define IDS_AUTHOR_EMAIL                57345
 #define IDS_URL_WEBSITE                 57346
 #define IDS_URL_HELP                    57347
@@ -297,8 +299,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        306
-#define _APS_NEXT_COMMAND_VALUE         32977
-#define _APS_NEXT_CONTROL_VALUE         1154
+#define _APS_NEXT_COMMAND_VALUE         32978
+#define _APS_NEXT_CONTROL_VALUE         1155
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
